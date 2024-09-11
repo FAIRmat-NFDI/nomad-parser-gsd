5051e125cf70812e2a34612ea23585ee5876fdc9
Bernadette Mohr <bernadette.mohr@physik.hu-berlin.de>
2024-09-11 11:11:29 +0200

    Fixed AssertionError: no matching parser

--- src/nomad_parser_gsd/parsers/__init__.py
+++ src/nomad_parser_gsd/parsers/__init__.py
@@ -15,5 +15,5 @@ parser_entry_point = GSDParserEntryPoint(
     name='GSDParser',
     description='Parser for the GSD file format, the native file format for HOOMD-blue. (https://gsd.readthedocs.io/en/v3.3.1/, https://glotzerlab.engin.umich.edu/hoomd-blue/)',
     mainfile_name_re='.*\.gsd.*',
-    mainfile_contents_re=r'GSD version [\d\.]*',
+    mainfile_binary_header_re=r'GSD version [\d\.]*',
 )
